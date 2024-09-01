a940777b674f45e51d7d2c510e562094def57f82
Rodrigo Vieira <rodrigo@slayer.tech>
2024-09-01 16:13:59 -0300

    🚧 Prepare autoboot

---
+++ Console.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * --------------------------------------------------------------------------
+ * Bootgly PHP Framework
+ * Developed by Rodrigo Vieira (@rodrigoslayertech)
+ * Copyright 2023-present
+ * Licensed under MIT
+ * --------------------------------------------------------------------------
+ */
+
+namespace Console;
+
+
+abstract class App // TODO
+{
+   // * Config
+   // ...
+
+   // * Data
+   // ...
+
+   // * Meta
+   // ...
+
+   public function autoboot ()
+   {
+      // TODO
+   }
+}
--- autoload.php
+++ autoload.php
@@ -1,2 +1,40 @@
 <?php
-// TODO
+/*
+ * --------------------------------------------------------------------------
+ * Bootgly PHP Framework
+ * Developed by Rodrigo Vieira (@rodrigoslayertech)
+ * Copyright 2023-present
+ * Licensed under MIT
+ * --------------------------------------------------------------------------
+ */
+
+// ?
+if (defined('CONSOLE_ROOT_BASE') === true) {
+   return;
+}
+
+// !
+define('CONSOLE_ROOT_BASE', __DIR__);
+define('CONSOLE_ROOT_DIR', __DIR__ . DIRECTORY_SEPARATOR);
+if (defined('CONSOLE_WORKING_BASE') === false) {
+   define('CONSOLE_WORKING_BASE', CONSOLE_ROOT_BASE);
+   define('CONSOLE_WORKING_DIR', CONSOLE_ROOT_DIR);
+}
+
+define('CONSOLE_VERSION', '0.1.0-alpha');
+
+// ! Bootables ([0-9]) || (-[a-z]) || ([0-9]-[a-z])
+// -- nothing --
+
+// ! Classes ([A-Z])
+// App (Application Programming Interface)
+spl_autoload_register (function (string $class) {
+   $paths = explode('\\', $class);
+   $file = implode('/', $paths) . '.php';
+
+   $included = @include(CONSOLE_WORKING_DIR . $file);
+
+   if ($included === false && CONSOLE_ROOT_DIR !== CONSOLE_WORKING_DIR) {
+      @include(CONSOLE_ROOT_DIR . $file);
+   }
+});
